e85f238aab31895077538ddde5af496360cacaf5
Hassan Alizadeh <alizadeh118@gmail.com>
2024-09-21 05:36:18 +0330

    feat: update theme context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import './index.css'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+import { ApolloProvider } from '@apollo/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+
+import { ThemeProvider } from '@/context/ThemeContext.tsx'
+import client from '@/graphql/client.ts'
+import router from '@/router/Routes'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ThemeProvider>
+            <ApolloProvider client={client}>
+                <RouterProvider router={router} />
+            </ApolloProvider>
+        </ThemeProvider>
+    </StrictMode>,
 )
